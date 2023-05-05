736c62599fb7b511adec7f864ea2d91db6856ed1
Anton Meshchaninov <tonymsnv@gmail.com>
2023-05-05 17:06:58 +0300

    Added bcrypt

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 import mongoose from 'mongoose'
 import { validationResult } from 'express-validator'
 
 // Validation
 import { registerValidation } from './validations/auth.js'
 
+// Models
+import UserModel from './models/User.js'
+
 // Connecting to DB
 mongoose
   .connect(
-    'mongodb+srv://admin:1234567As@personal-blog.52bvf2n.mongodb.net/?retryWrites=true&w=majority',
+    'mongodb+srv://admin:1234567As@personal-blog.52bvf2n.mongodb.net/blog?retryWrites=true&w=majority',
   )
   .then(() => console.log('DB is ok.'))
   .catch((error) => console.log(error))
@@ -20,16 +24,27 @@ const app = express()
 app.use(express.json())
 
 // Endpoints
-app.post('/auth/register', registerValidation, function (req, res) {
+app.post('/auth/register', registerValidation, async function (req, res) {
   const errors = validationResult(req)
 
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array())
   }
 
-  res.json({
-    success: true,
+  const password = req.body.password
+  const salt = await bcrypt.genSalt(10)
+  const passwordHash = await bcrypt.hash(password, salt)
+
+  const doc = new UserModel({
+    fullName: req.body.fullName,
+    email: req.body.email,
+    passwordHash,
+    avatarUrl: req.body.avatarUrl,
   })
+
+  const user = await doc.save()
+
+  res.json(user)
 })
 
 // Start server
--- validations/auth.js
+++ validations/auth.js
@@ -1,8 +1,11 @@
 import { body } from 'express-validator'
 
 export const registerValidation = [
-  body('email').isEmail(),
-  body('password').isLength({ min: 6, max: 20 }),
-  body('fullName').isLength({ min: 3 }),
-  body('avatarUrl').optional().isURL(),
+  body('email', 'Неверный формат почты e-mail').isEmail(),
+  body('password', 'Пароль должен содержать от 6 до 20 символов').isLength({
+    min: 6,
+    max: 20,
+  }),
+  body('fullName', 'Необходимо указать полное имя').isLength({ min: 3 }),
+  body('avatarUrl', 'Неверная ссылка на фото').optional().isURL(),
 ]
