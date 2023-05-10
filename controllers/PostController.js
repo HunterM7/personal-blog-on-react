634007a5045d6ebf4188e07bf37ca03b810c73f6
Anton Meshchaninov <tonymsnv@gmail.com>
2023-05-10 16:18:57 +0300

    Made PostController

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,1 +1,22 @@
-export async function createPost(req, res) {}
+// Models
+import PostModel from '../models/Post.js'
+
+export async function createPost(req, res) {
+  try {
+    const doc = new PostModel({
+      title: req.body.title,
+      text: req.body.text,
+      tags: req.body.tags,
+      imageUrl: req.body.imageUrl,
+      user: req.userId,
+    })
+
+    const post = await doc.save()
+
+    res.status(200).json(post)
+  } catch (error) {
+    console.log(error)
+
+    res.status(500).json({ message: 'Не удалось создать статью' })
+  }
+}
