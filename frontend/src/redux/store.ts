3e56e5cf9b8f1ebfe446bb3c618a59744976c8a7
Anton Meshchaninov <tonymsnv@gmail.com>
2023-05-12 18:32:53 +0300

    Typed store

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux'
 
 // Reducers
 import { postsReducer } from 'redux/slices/posts'
@@ -10,8 +10,14 @@ const store = configureStore({
   },
 })
 
+// RootState
+export type TRootState = ReturnType<typeof store.getState>
+
 // Dispatch typing
 export type AppDispatch = typeof store.dispatch
 export const useAppDispatch: () => AppDispatch = useDispatch
+
+// Selector typing
+export const useAppSelector: TypedUseSelectorHook<TRootState> = useSelector
 
 export default store
